108a581c199aa03e264ed1e58b7d66d280dafbca
Surjith Vijayakumar <rvdsurjith@gmail.com>
2024-09-12 21:54:36 +0530

    added Hello 1

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App(){
 
   return(
     <BrowserRouter>
+    <p>Hello 1</p>
       <Switch>
         <Route exact path='/' component={Home}></Route>
         <Route exact path='/register' component={Register}></Route>
